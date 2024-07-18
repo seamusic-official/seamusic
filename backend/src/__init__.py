089c99ad63ee4bb4c6403c9fe2fd36e3bf95eb90
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-07-19 00:11:47 +0300

    исправлена ошибка при создании миграций

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,8 +1,10 @@
-"""Not deleted this code because it is used in the alembic."""
+"""Не удаляй этот код, поскольку он используется при создании миграций в alembic.
+Сюда мы кидаем импорты на модели для миграций
+"""
+
 from src.beatpacks.models import Beatpack
 from src.squads.models import Squad
 from src.auth.models import User, ProducerProfile, ArtistProfile
 from src.beats.models import Beat
 from src.soundkits.models import Soundkit
 from src.tags.models import Tag
-
